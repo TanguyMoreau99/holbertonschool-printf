9546135d854fddfcc40054c57d931436f933ef71
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-25 15:43:40 +0100

    add the first main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int _putchar(char prm_c);
+int _printf(const char *prm_ptr_format, ...);
+void print_char(char prm_c);
+void print_string(char *prm_ptr_string);
+void print_percent(char prm_c);
+
+#endif
