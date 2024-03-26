f579f85f6f8baaa23a7040ecc92c1fddd0fa5d4a
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-26 10:54:40 +0100

    fix syntax error

--- get_print_function.c
+++ get_print_function.c
@@ -12,13 +12,13 @@
 
 int (*get_print_function(char *ptr_specifier))(va_list)
 {
-	print spe[] = { 
+	print spe[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
 		{"d", print_decimal},
 		{"i", print_integer},
-		{NULL, NULL},
+		{NULL, NULL}
+		};
 	int i = 0;
-	}
 }
