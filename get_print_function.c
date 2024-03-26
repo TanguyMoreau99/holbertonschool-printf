21d318500b1aad19035c20f9e87076239f219804
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-26 11:41:27 +0100

    add a part of code of the function

--- get_print_function.c
+++ get_print_function.c
@@ -15,10 +15,20 @@ int (*get_print_function(char *ptr_specifier))(va_list)
 	print spe[] = {
 		{"c", print_char},
 		{"s", print_string},
-		{"%", print_percent},
+		{'%', print_percent},
 		{"d", print_decimal},
 		{"i", print_integer},
 		{NULL, NULL}
 		};
 	int i = 0;
 }
+	while (spe[i].specifier != NULL)
+	{
+		if (spe[i].specifier[0] == ptr_specifier[0])
+		{
+			return (spe[i].print_function);
+		}
+		i++;
+	}
+
+	return (NULL);
