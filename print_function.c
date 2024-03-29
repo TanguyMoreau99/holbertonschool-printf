50b1716ce59ba620c7a8721aaf0782b2189287b3
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-29 11:08:46 +0100

    add comments on integer function and move main.c

--- print_function.c
+++ print_function.c
@@ -65,11 +65,14 @@ int print_decimal(va_list ptr_var)
 	long memory = 0;
 
 	if (integer == 0)
+	/* If the number is 0, just print 0 */
 	{
 		_putchar(0);
 	}
 
 	if (integer < 0)
+	/* If the integer is negative, print the '-'
+	multiply the integer *-1 to access the while loop */
 	{
 		_putchar('-');
 		count = (count + 1);
@@ -77,12 +80,21 @@ int print_decimal(va_list ptr_var)
 	}
 
 	while (integer > 0)
+	/* To get digits of the integer we use %10 and put the digit
+	into the variable memory. Once we stock a digit, we divide
+	the integer by 10 to continue the loop until
+	the integer isn't > 0 anymore */
 	{
 		digit = integer %10;
 		memory = (memory * 10) + digit;
 		integer = integer /10;
 	}
 	while (memory > 0)
+	/* Now, we have our integer into our memory but in reverse
+	So, we use modulo 10 to get that last number of memory, which
+	is the first number of integer, then we print it.
+	We divide memory by 10 to continue the loop until
+	memory isn't > 0 anymore */
 	{
 		digit = memory %10;
 		_putchar(digit + '0');
